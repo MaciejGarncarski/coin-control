d46af49d7568e9c192605adfcdca000535b4f4fb
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-03-11 11:07:33 +0100

    chore: change db port

--- api/lib/db.ts
+++ api/lib/db.ts
@@ -2,5 +2,5 @@ import postgres from 'postgres'
 import { env } from '../src/config/env.js'
 
 export const db = postgres(
-  `postgres://${env.PG_USER}:${env.PG_PASS}@db:5432/budget_app`,
+  `postgres://${env.PG_USER}:${env.PG_PASS}@db:6432/budget_app`,
 )
