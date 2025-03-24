6e00cc6b35ce8d743d56254df2ca1a1f1d604a56
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-03-24 11:53:20 +0100

    chore: remove healthcheck

--- worker/src/app.ts
+++ worker/src/app.ts
@@ -1,5 +1,3 @@
-import { createServer } from "node:http";
-
 import { createExpiredSessionRemoverWorker } from "./workers/expired-session-remover.js";
 import { createEmailVerificationWorker } from "./workers/email-verification.js";
 import { createResetPasswordLinkWorker } from "./workers/reset-email-password-link.js";
@@ -9,10 +7,3 @@ createResetPasswordLinkWorker();
 createEmailVerificationWorker();
 createResetPasswordNotificationWorker();
 createExpiredSessionRemoverWorker();
-
-// healthcheck
-createServer(function (_, res) {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("OK");
-  res.end();
-}).listen(3005);
