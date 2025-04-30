827491d13181c83887af66ed8d71309e0246ff1f
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-04-30 12:15:17 +0200

    chore: increase rate limit

--- api/src/config/rate-limit.ts
+++ api/src/config/rate-limit.ts
@@ -3,6 +3,6 @@ import ms from 'ms'
 import { createRateLimiter } from '../lib/rate-limiter.js'
 
 export const globalRateLimit = createRateLimiter({
-  windowMs: ms('15 minutes'),
-  limit: 100,
+  windowMs: ms('5 minutes'),
+  limit: 1000,
 })
