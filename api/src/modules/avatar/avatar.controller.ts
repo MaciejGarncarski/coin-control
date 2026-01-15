337809366ce87192294db2b56bb4b374b703aa4c
Maciej Garncarski <maciejgarncarski@protonmail.com>
2026-01-15 11:31:03 +0100

    refactor: streams

--- api/src/modules/avatar/avatar.controller.ts
+++ api/src/modules/avatar/avatar.controller.ts
@@ -1,7 +1,6 @@
-import { createReadStream, createWriteStream } from 'node:fs'
+import { createWriteStream } from 'node:fs'
 import { mkdir, unlink } from 'node:fs/promises'
 import { join, normalize } from 'node:path'
-import { pipeline } from 'node:stream/promises'
 
 import { db } from '@shared/database'
 import { type Request, type Response } from 'express'
@@ -12,74 +11,60 @@ import { nanoid } from 'nanoid'
 import { env } from '../../config/env.js'
 import { ApiError } from '../../utils/api-error.js'
 
-const MAX_FILE_SIZE = 5_000_000 // 5 MB;
+const MAX_FILE_SIZE = 5_000_000
+const UPLOAD_DIR = normalize(join('avatar-upload'))
 
 export async function uploadUserAvatarHandler(req: Request, res: Response) {
   const userId = req.session.userId
+  const avatarId = nanoid()
+  const newAvatarName = `${avatarId}.jpg`
+  const safeFilePath = join(UPLOAD_DIR, newAvatarName)
+
+  await mkdir(UPLOAD_DIR, { recursive: true })
 
   const form = formidable({
     maxFileSize: MAX_FILE_SIZE,
-  })
-
-  const [_, files] = await form.parse(req)
-
-  const avatarFiles = files.avatar
-
-  if (!avatarFiles) {
-    throw new ApiError({
-      message: 'No image provided',
-      statusCode: status.BAD_REQUEST,
-    })
-  }
-
-  const avatar = avatarFiles[0]
-
-  if (!avatar) {
-    throw new ApiError({
-      message: 'No image provided',
-      statusCode: status.BAD_REQUEST,
-    })
-  }
-
-  const userData = await db.users.findFirst({
-    where: {
-      id: userId,
-    },
-    select: {
-      avatar_url: true,
+    fileWriteStreamHandler: () => {
+      return createWriteStream(safeFilePath)
     },
   })
-
-  const avatarFileName = userData?.avatar_url?.split(
-    `${env.API_URL}/avatars/`,
-  )[1]
 
   try {
-    await unlink(normalize(join('avatar-upload', avatarFileName || '')))
-    // eslint-disable-next-line no-empty
-  } catch {}
+    const [fields, files] = await form.parse(req)
+    const avatar = files.avatar?.[0]
 
-  const avatarId = nanoid()
-  await mkdir(normalize(join('avatar-upload')), { recursive: true })
-  const newAvatarName = `${avatarId}.jpg`
-  const safeFilePath = normalize(join('avatar-upload', newAvatarName))
+    if (!avatar) {
+      throw new ApiError({
+        message: 'No image provided',
+        statusCode: status.BAD_REQUEST,
+      })
+    }
 
-  await pipeline(
-    createReadStream(avatar.filepath),
-    createWriteStream(safeFilePath),
-  )
+    const userData = await db.users.findFirst({
+      where: { id: userId },
+      select: { avatar_url: true },
+    })
 
-  const avatarURL = `${env.API_URL}/avatars/${newAvatarName}`
+    if (userData?.avatar_url) {
+      const oldFileName = userData.avatar_url.split(
+        `${env.API_URL}/avatars/`,
+      )[1]
+      if (oldFileName) {
+        await unlink(join(UPLOAD_DIR, oldFileName)).catch(() => {})
+      }
+    }
 
-  await db.users.update({
-    where: {
-      id: userId,
-    },
-    data: {
-      avatar_url: avatarURL,
-    },
-  })
+    const avatarURL = `${env.API_URL}/avatars/${newAvatarName}`
+    await db.users.update({
+      where: { id: userId },
+      data: { avatar_url: avatarURL },
+    })
 
-  res.status(status.OK).send({ message: 'ok', filename: newAvatarName })
-  return
+    return res
+      .status(status.OK)
+      .send({ message: 'ok', filename: newAvatarName })
+  } catch (err) {
+    await unlink(safeFilePath).catch(() => {})
+    throw err
+  }
 }
