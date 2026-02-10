9e9bb28a37053418141a461162a7e2f24225d80e
Maciej Garncarski <maciejgarncarski@protonmail.com>
2026-02-10 14:17:45 +0100

    fix: type error

--- shared/database/src/index.ts
+++ shared/database/src/index.ts
@@ -1,2 +1,3 @@
 export { db } from './client.js' // exports instance of prisma
+export { Prisma } from './generated/prisma/client.js'
 export * from './generated/prisma/client.js' // exports generated types from prisma
