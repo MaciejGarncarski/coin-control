e0acb8e4b10b496dd4c3032909b4fd66cc71c848
Maciej Garncarski <maciejgarncarski@protonmail.com>
2026-02-10 13:54:31 +0100

    style: format code

--- api/src/utils/typed-request.ts
+++ api/src/utils/typed-request.ts
@@ -4,12 +4,14 @@ export interface TypedRequestBody<T> extends Request {
   body: T
 }
 
-export interface TypedRequestParams<T extends Record<string, string>>
-  extends Request {
+export interface TypedRequestParams<
+  T extends Record<string, string>,
+> extends Request {
   params: T
 }
 
-export interface TypedRequestQuery<T extends Record<string, string>>
-  extends Request {
+export interface TypedRequestQuery<
+  T extends Record<string, string>,
+> extends Request {
   query: T
 }
